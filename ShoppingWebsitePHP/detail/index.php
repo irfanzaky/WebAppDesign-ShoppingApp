02ff78475feccfda6eea355d982535f6adbc3c1f
Irfan Zaky Harlen <irfanzakyharlen@gmail.com>
2020-11-11 07:21:31 +0800

    add abit of style

--- ShoppingWebsitePHP/detail/index.php
+++ ShoppingWebsitePHP/detail/index.php
@@ -101,5 +101,44 @@
 
     <!-- Scripts -->
     <script></script>
+    <style>
+      input[type=text], select {
+        width: 60%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+      }
+
+      input[type=number], select {
+        width: 60%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+      }
+
+
+      input[type=submit] {
+        width: 80%;
+        background-color: #4CAF50;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+      }
+
+      input[type=submit]:hover {
+        background-color: #45a049;
+      }
+
+}
+</style>
   </body>
 </html>
